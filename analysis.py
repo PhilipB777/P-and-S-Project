1b7df86593bd22b6a0c4c58359bf4b7bdcacc060
PhilipB777 <philipgbrady@gmail.com>
2020-04-26 18:55:33 +0100

    The Iris Data Set

--- analysis.py
+++ analysis.py
@@ -2,3 +2,14 @@
 # The Programming and Scripting Project 2020.
 # This program displays an analysis of Fisher's
 # Iris Data Set.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+irisdata = pd.read_csv('iris.csv')
+plt.hist(irisdata['sepal_length'])
+plt.title("Iris Sepal Length")
+plt.xlabel("Sepal Length in cm")
+plt.ylabel("Irises")
+plt.savefig("Sepal Length Histogram.png")
+plt.show()
