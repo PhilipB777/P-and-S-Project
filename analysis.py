b77abc48ddfdee20af1384cfa2bc02c8f134bf7e
PhilipB777 <philipgbrady@gmail.com>
2020-04-27 20:10:20 +0100

    Analysis of the Iris Data Set

--- analysis.py
+++ analysis.py
@@ -16,16 +16,32 @@ irisData = pd.read_csv('iris.csv')
 #with open ('Iris Data Summary.txt', 'w') as f:
 #    f.write(text)
 
-
+# Initialise the variable with an empty string.
 irisVar = ""
+# Use this while loop to allow the user to exit the program.
 while irisVar != "E":
+    # Use this nested while loop to allow the user to view multiple histograms.
     while irisVar not in ("Sepal length", "Sepal width", "Petal length", "Petal width", "E"):
+        # The capitalise function allows the user to enter the variable in any case.
         irisVar = input("Enter the Iris Variable Name to view a histogram or press 'e' to Exit: ").capitalize()
+    # Checks that the user hasn't entered e for exit.
     if irisVar != "E":
+        # Use the pyplot module to create a histogram of the input iris variable.
+        # Use red for a better visual display.
         plt.hist(irisData[irisVar], color = "red")
+        # Add a title to the histogram.
+        # The title is a concatenation with the input iris variable.
         plt.title("Iris " + irisVar)
+        # Add an x label to the histogram.
+        # The x label is a concatenation with the input iris variable.
         plt.xlabel(irisVar + " in cm")
+        # Add a y label Irises to the histogram.
         plt.ylabel("Irises")
+        # Save the figure of the histogram in the users folder.
         plt.savefig(irisVar + " Histogram.png")
+        # Clear the figure of the histogram to allow the user to save a new
+        # histogram of another variable.
         plt.clf()
+        # Reset the variable to an empty string to allow the user to input
+        # another variable.
         irisVar = ""
