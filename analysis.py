53b8a3c1d93174c15f7a4fe13f7cc1ce05baec9a
PhilipB777 <philipgbrady@gmail.com>
2020-04-27 19:45:21 +0100

    Iris Sepal Length Histogram

--- analysis.py
+++ analysis.py
@@ -6,12 +6,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-irisdata = pd.read_csv('iris.csv')
+irisData = pd.read_csv('iris.csv')
 
 # Use the describe function to obtain a summary of the Iris Data.
-summary = irisdata.describe()
+#summary = irisData.describe()
 # Convert the summary to a string and round to one decimal place for a better visual.
-text = str(summary.round(1))
+#text = str(summary.round(1))
 # Create a new file and write the text summary to it.
-with open ('Iris Data Summary.txt', 'w') as f:
-    f.write(text)
+#with open ('Iris Data Summary.txt', 'w') as f:
+#    f.write(text)
+
+
+irisVar = ""
+while irisVar != "E":
+    while irisVar not in ("Sepal length", "Sepal width", "Petal length", "Petal width", "E"):
+        irisVar = input("Enter the Iris Variable Name to view a histogram or press 'e' to Exit: ").capitalize()
+    if irisVar != "E":
+        plt.hist(irisData[irisVar], color = "red")
+        plt.title("Iris " + irisVar)
+        plt.xlabel(irisVar + " in cm")
+        plt.ylabel("Irises")
+        plt.savefig(irisVar + " Histogram.png")
+        plt.clf()
+        irisVar = ""
