afd8e52d8979e5ad5c7f6ed0557ed5fe256882cd
PhilipB777 <philipgbrady@gmail.com>
2020-04-28 15:18:57 +0100

    Analysis of the Iris Data Set

--- analysis.py
+++ analysis.py
@@ -3,18 +3,21 @@
 # This program displays an analysis of Fisher's
 # Iris Data Set.
 
+# Import the required libraries.
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Use the pandas library to read in the data from the csv file.
 irisData = pd.read_csv('iris.csv')
 
 # Use the describe function to obtain a summary of the Iris Data.
-#summary = irisData.describe()
+summary = irisData.describe()
 # Convert the summary to a string and round to one decimal place for a better visual.
-#text = str(summary.round(1))
+text = str(summary.round(1))
 # Create a new file and write the text summary to it.
-#with open ('Iris Data Summary.txt', 'w') as f:
-#    f.write(text)
+with open ('Iris Data Summary.txt', 'w') as f:
+    f.write(text)
 
 # Initialise the variable with an empty string.
 irisVar = ""
@@ -22,13 +25,13 @@ irisVar = ""
 while irisVar != "E":
     # Use this nested while loop to allow the user to view multiple histograms.
     while irisVar not in ("Sepal length", "Sepal width", "Petal length", "Petal width", "E"):
-        # The capitalise function allows the user to enter the variable in any case.
+        # The capitalise function will format the input to the required case.
         irisVar = input("Enter the Iris Variable Name to view a histogram or press 'e' to Exit: ").capitalize()
     # Checks that the user hasn't entered e for exit.
     if irisVar != "E":
         # Use the pyplot module to create a histogram of the input iris variable.
-        # Use red for a better visual display.
-        plt.hist(irisData[irisVar], color = "red")
+        # Use ten bins and the colour red for an optimal visual display.
+        plt.hist(irisData[irisVar], bins = 10, color = "red")
         # Add a title to the histogram.
         # The title is a concatenation with the input iris variable.
         plt.title("Iris " + irisVar)
